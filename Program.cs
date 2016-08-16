12b208eaa5717d20b95be560570e53f106b281e0
Jake Overall <joverall22@gmail.com>
2016-08-16 09:59:24 -0600

    I saw your number

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int MIN = 1;
+            const int MAX = 100;
+            var guessing = true;
+            var rand = new Random();
+            int myRandNum = rand.Next(MIN, MAX);
+
+            Console.WriteLine($"Can you guess my number???? It's somewhere between {MIN} - {MAX}");
+            Console.WriteLine(myRandNum);
+            while (guessing)
+            {
+                var guessInput = Console.ReadLine();
+
+                int guess;
+                int.TryParse(guessInput, out guess);
+
+                if (guess == myRandNum)
+                {
+                    Console.WriteLine("I BOW TO YOU");
+                    guessing = false;
+                    return;
+                }
+                else if (guess < myRandNum)
+                {
+                    Console.WriteLine("A BIT TO LOW");
+                }
+                else if (guess > myRandNum)
+                {
+                    Console.WriteLine("TO HIGH");
+                }
+                Console.WriteLine("Nope Guess Again");
+            }
+
+
+        }
+    }
+}
